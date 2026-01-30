177cd3d224be8e88673e4209d90af313b42bf6c7
Florian van der Galiën <hallo@flori.dev>
2026-01-30 12:11:26 +0100

    chore(deps): update dependencies and add Tailwind CSS support
    - Added @astrojs/node, @astrojs/vue, and tailwindcss to dependencies.
    - Introduced global CSS file for Tailwind CSS imports.
    - Updated tsconfig.json to include path mapping and JSX preservation.
    - Cleaned up HTML structure in index.astro for consistency.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+
+import vue from '@astrojs/vue'
+import node from '@astrojs/node'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [vue()],
+
+  adapter: node({
+    mode: 'standalone',
+  }),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+})
